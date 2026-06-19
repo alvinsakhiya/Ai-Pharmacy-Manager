209fad5eaaa9622e589cb12f528916a942b09191
Alvin Sakhiya <alvinsakhiya26@gmail.com>
2026-06-19 16:47:59 +0100

    test(phase2): relax login throttle for e2e workflow

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_THROTTLE_RATES": {
-        "login": "10/min",
+        "login": os.getenv("LOGIN_THROTTLE_RATE", "10/min"),
     },
 }
 
